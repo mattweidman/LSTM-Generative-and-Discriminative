3db8f43f3c3c8d0a85cc7103f14be6e0900937e1
Matthew Weidman <weidman.matthew@gmail.com>
2016-08-18 16:46:05 -0400

    minibatches made it work a little better

--- generator.py
+++ generator.py
@@ -48,26 +48,33 @@ if __name__ == "__main__":
     network.add_layer(LSTM_layer(hidden_size, output_size))
 
     # construct the input
-    seq_length = 3
-    num_examples = 100
-    X_once = np.random.randn(num_examples, input_size)
-    X_sequence = np.zeros((num_examples, seq_length, input_size))
-    for i in range(num_examples):
-        randind = random.randint(0, num_chars-1)
-        for j in range(seq_length):
-            X_sequence[i,j,randind] = 1
+    seq_length = 5
+    num_examples = 1000
+    def gen_x_sequence():
+        X_sequence = np.zeros((num_examples, seq_length, input_size))
+        for i in range(num_examples):
+            randind = random.randint(0, num_chars-1)
+            for j in range(seq_length):
+                X_sequence[i,j,randind] = 1
+        return X_sequence
 
     # loss function and its gradient
+    def get_n(h_):
+        n = 1
+        for dim in h_.shape[:-1]:
+            n *= dim
+        return n
     def loss(h_, x_):
-        return 1/(2*h_.shape[0]) * np.sum((h_-x_)**2)
+        return 1/(2*get_n(h_)) * np.sum((h_-x_)**2)
     def dloss(h_, x_):
-        return 1/h_.shape[0] * (h_-x_)
-    dloss_i = lambda h_, i_: dloss(h_, X_sequence[:,i_,:])
+        return 1/get_n(h_) * (h_-x_)
 
     # train
     num_epochs = 1000
-    learning_rate = 3
+    learning_rate = 10
     for i in range(num_epochs):
+        X_sequence = gen_x_sequence()
+        dloss_i = lambda h_, i_: dloss(h_, X_sequence[:,i_,:])
         grad = network.BPTT_one2one(X_sequence, dloss_i)
         network.update_theta(grad, learning_rate)
         outp = network.forward_prop_one2one(X_sequence)
@@ -77,10 +84,11 @@ if __name__ == "__main__":
     # use the LSTM
     def char_to_matx(c, length=seq_length):
         return [char_to_vec(c)] * length
-    inp = np.array([char_to_matx('a'), char_to_matx('b'), char_to_matx('c')])
-    print(tensor_to_strings(inp))
-    sequence_tensor = network.forward_prop_one2one(inp)
-    print(tensor_to_strings(sequence_tensor))
+    inp = np.array([char_to_matx(chr(c)) for c in range(97, 123)])
+    print(inp.shape)
+    outp = network.forward_prop_one2one(inp)
+    for i in range(inp.shape[0]):
+        print("%s\t%s" % (chr(i+97), matrix_to_string(outp[i])))
 
     '''# backprop for multiple layers
     input_size = 5
